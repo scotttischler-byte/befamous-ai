265f6dbb262b6f297b17b8d864cb420899f09bc1
scott tischler <scott@scotts-MacBook-Pro-2.local>
2026-04-14 15:53:30 -0500

    force redirect / to /jjlegal

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,4 @@
-import Link from "next/link";
-
-export default function Home() {
-return (
-<main style={{ padding: 24 }}>
-<h1>BeFamous Growth Engine</h1>
-<div style={{ display: "flex", gap: 16, marginTop: 20 }}>
-<Link href="/jjlegal">JJ Legal</Link>
-<Link href="/tygibson">Ty Gibson</Link>
-</div>
-</main>
-);
+import { redirect } from "next/navigation";
+export default function HomePage() {
+redirect("/jjlegal");
 }
